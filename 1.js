c73f15227dfc4c128a6a0d3e55516172187e9c78
AryaPatel0209 <aryapatel72186@gmail.com>
2026-06-03 23:31:15 +0530

    2

---
+++ 1.js
@@ -0,0 +1,14 @@
+// const Accountid = 144232
+// let accountemail = "arya@1213"
+// var accountpass = "123456"
+// accountcity = "jaipur"
+
+// accountemail = "adi123"
+// accountpass ="123155"
+// accountcity = "ahmedabad"
+
+// // console.log(Accountid)
+
+// console.table([accountemail, accountpass, accountcity]);
+
+// Prefer not to use var
---
+++ 2.js
@@ -0,0 +1,15 @@
+let score = "3"
+// console.log(typeof score);
+
+let valueinnumber = Number(score)
+console.log(typeof score);
+
+let somenumber = 33
+
+let stringnumber = String(somenumber)
+console.log(typeof stringnumber);
+
+let stringname = "Arya"
+
+let boolean = Boolean(stringname)
+console.log( boolean);
